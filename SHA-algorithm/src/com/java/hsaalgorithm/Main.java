940aca9b2bb95fe8b2e2bb98e3d7ec898106d62d
Prosphi06 <mathebulas244@gmail.com>
2022-01-03 17:43:55 +0200

    updated SHA256

--- SHA-algorithm/src/com/java/hsaalgorithm/Main.java
+++ SHA-algorithm/src/com/java/hsaalgorithm/Main.java
@@ -2,7 +2,25 @@ package com.java.hsaalgorithm;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input");
+        String input = sc.nextLine();
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        md.update(input.getBytes());
+
+        byte[] digest = md.digest();
+
+        System.out.println(digest);
+
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i< digest.length; i++){
+            hexString.append(Integer.toHexString(0xFF & digest[i]));
+            // hexString.append(String.format("%02x", i));
+        }
+        System.out.println("Hex decimal format : " +hexString.toString());
     }
 }
